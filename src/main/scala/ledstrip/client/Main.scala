bd6d00454f3629112333e84c76af6bdb027b7f4e
Pierre Kisters <pierrekisters@gmail.com>
2019-10-05 17:43:46 +0200

    a client example

--- src/main/scala/ledstrip/client/Main.scala
+++ src/main/scala/ledstrip/client/Main.scala
@@ -1,22 +1,68 @@
 package ledstrip.client
 
+import cats.effect.ExitCode
 import com.github.mbelling.ws281x.Color
 import ledstrip.ColorRule
-import monix.eval.Task
+import monix.eval.{Task, TaskApp}
 import monix.execution.Scheduler
-import monix.execution.Scheduler.Implicits.global
 import org.http4s.circe.CirceEntityEncoder._
+import org.http4s.client.Client
 import org.http4s.client.blaze.BlazeClientBuilder
-import org.http4s.{Request, Uri}
+import org.http4s.{Method, Request, Uri}
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    BlazeClientBuilder[Task](Scheduler.global).resource.use { client =>
-      val request: Request[Task] =
-        Request(uri = Uri.unsafeFromString("http://10.1.15.178:8080/colors"))
-          .withEntity(List(ColorRule(None, new Color(255, 0, 0))))
+object Main extends TaskApp {
+  def sendColors(colorRules: List[ColorRule])(implicit client: Client[Task]): Task[Unit] = {
+    val request: Request[Task] =
+      Request(
+        method = Method.POST,
+        uri = Uri.unsafeFromString("http://10.1.15.178:8080/colors")
+      )
+        .withEntity(colorRules)
 
-      client.successful(request)
-    }
+    client.successful(request).map(_ => ())
   }
+
+  override def run(args: List[String]): Task[ExitCode] =
+    for {
+      _ <- BlazeClientBuilder[Task](Scheduler.global).resource.use { implicit client =>
+        /*def cycle(color: Color): Task[Unit] = {
+          for {
+            _ <- sendColors(List(ColorRule(None, color)))
+            _ <- cycle(new Color(if (color.getBlue > 0) 255 else 0, if (color.getRed > 0) 255 else 0, if (color.getGreen > 0) 255 else 0))
+          } yield ()
+        }
+
+        cycle(new Color(255, 0, 0))*/
+
+        (for {
+          _ <- Task.sequence {
+            for (i <- 0 until 150) yield
+              sendColors(List(ColorRule(None, Color.BLACK), ColorRule(Some((0 to i).toList), Color.BLUE)))
+          }
+          _ <- Task.sequence {
+            for (i <- 0 until 150) yield
+              sendColors(List(ColorRule(None, Color.BLACK), ColorRule(Some((i to 150).toList), Color.BLUE)))
+          }
+
+          _ <- Task.sequence {
+            for (i <- 0 until 150) yield
+              sendColors(List(ColorRule(None, Color.BLACK), ColorRule(Some((0 to i).toList), Color.RED)))
+          }
+          _ <- Task.sequence {
+            for (i <- 0 until 150) yield
+              sendColors(List(ColorRule(None, Color.BLACK), ColorRule(Some((i to 150).toList), Color.RED)))
+          }
+
+          _ <- Task.sequence {
+            for (i <- 0 until 150) yield
+              sendColors(List(ColorRule(None, Color.BLACK), ColorRule(Some((0 to i).toList), Color.GREEN)))
+          }
+          _ <- Task.sequence {
+            for (i <- 0 until 150) yield
+              sendColors(List(ColorRule(None, Color.BLACK), ColorRule(Some((i to 150).toList), Color.GREEN)))
+          }
+        } yield ()).loopForever
+      }.onErrorRestartIf(_ => true)
+    } yield
+      ExitCode.Success
 }
